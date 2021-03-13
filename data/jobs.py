9bcd30e09a366566b99404f4783f6f0576bb94aa
Egor <edkuz11@yandex.ru>
2021-03-13 17:56:49 +0300

    Index page

--- data/jobs.py
+++ data/jobs.py
@@ -9,6 +9,7 @@ class Jobs(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    user_team_leader = orm.relation("User")
     job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
+from data.jobs import Jobs
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "very_long_secret_key"
+
+
+@app.route("/")
+def index():
+    session = db_session.create_session()
+    jobs = session.query(Jobs).all()
+    return render_template("index.html", title="Works log", jobs=jobs)
 
 
 def main():
