4f0dbaa21b5c053c4201fc5cee87e4cfa759accd
Egor <edkuz11@yandex.ru>
2021-03-15 20:07:36 +0300

    Adding user login

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 
 from data import db_session
 from data.jobs import Jobs
@@ -52,6 +52,19 @@ def register():
         session.commit()
         return redirect("/login")
     return render_template("register.html", title=title, form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template("login.html", title="Авторизация", message="Неправильный логин или пароль", form=form)
+    return render_template("login.html", title="Авторизация", form=form)
 
 
 def main():
