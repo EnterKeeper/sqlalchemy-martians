1ac9300fdd884e20a7680fec9615446674d625a3
Egor <edkuz11@yandex.ru>
2021-03-17 20:14:36 +0300

    API creating job

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,5 +1,6 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
+import sqlalchemy.exc
 
 from . import db_session
 from .jobs import Jobs
@@ -12,6 +13,7 @@ blueprint = flask.Blueprint(
 )
 
 FIELDS = ("id", "team_leader", "job", "work_size", "collaborators", "start_date", "end_date", "is_finished", "category")
+REQUIRED_FIELDS = ("team_leader", "category")
 
 
 @blueprint.route("/api/jobs")
@@ -30,9 +32,27 @@ def get_one_job(job_id):
     session = db_session.create_session()
     job = session.query(Jobs).filter(Jobs.id == job_id).first()
     if not job:
-        return jsonify({"error": "not found"})
+        return jsonify({"error": "Not found"})
     return jsonify(
         {
             "job": job.to_dict(only=FIELDS[1:])
         }
     )
+
+
+@blueprint.route("/api/jobs", methods=["POST"])
+def create_one_job():
+    data = request.json
+    if not data:
+        return jsonify({"error": "Empty request"})
+    if not all(field in data for field in REQUIRED_FIELDS):
+        return jsonify({"error": "Bad request"})
+    session = db_session.create_session()
+    field_values = {field: data[field] for field in FIELDS if field in data}
+    job = Jobs(**field_values)
+    try:
+        session.add(job)
+        session.commit()
+    except sqlalchemy.exc.IntegrityError:
+        return jsonify({"error": "Id already exists"})
+    return jsonify({"success": "OK"})
